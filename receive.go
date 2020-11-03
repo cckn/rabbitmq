4195772b0abc2202fadfb17a8fab5ac900ba9d03
cckn <pds@enitt.co.kr>
2020-11-03 16:48:40 +0900

    feature - receive.go

--- receive.go
+++ receive.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://admin:admin@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message : %s", d.Body)
+		}
+	}()
+
+	log.Printf("[*] Waiting fo messages. To exit press CTRL+C")
+	<-forever
+}
